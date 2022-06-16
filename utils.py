ed28bf6b595d1f555925ad475ac14e1cf423213b
Leryud <leo.innocenzi@gmail.com>
2022-06-16 13:48:53 +0200

    added utils

---
+++ utils.py
@@ -0,0 +1,27 @@
+import cv2
+
+def setup_tracker(ttype):
+    tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
+    tracker_type = tracker_types[ttype]
+
+    if tracker_type not in tracker_types:
+        print('Incorrect tracker name')
+        print('Available trackers are:')
+        for t in tracker_types:
+            print(t)
+
+    else :
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.TrackerBoosting_create()
+        if tracker_type == 'MIL':
+            tracker = cv2.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        if tracker_type == 'TLD':
+            tracker = cv2.TrackerTLD_create()
+        if tracker_type == 'MEDIANFLOW':
+            tracker = cv2.TrackerMedianFlow_create()
+        if tracker_type == 'GOTURN':
+            tracker = cv2.TrackerGOTURN_create()
+
+    return tracker
